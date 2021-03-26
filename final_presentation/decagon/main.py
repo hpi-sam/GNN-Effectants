c0365f4b0fe1a864d789f6b190055da64766661f
witsyke <witzke.simon@gmail.com>
2021-03-26 23:07:26 +0100

    Adds network processing to decagon

--- final_presentation/decagon/main.py
+++ final_presentation/decagon/main.py
@@ -4,6 +4,7 @@ from operator import itemgetter
 from itertools import combinations
 import time
 import os
+import csv
 
 import tensorflow as tf
 import numpy as np
@@ -113,50 +114,231 @@ def construct_placeholders(edge_types):
 # (3) Train & test the model.
 ####
 
-val_test_size = 0.05
-n_genes = 500
-n_drugs = 400
-n_drugdrug_rel_types = 3
-gene_net = nx.planted_partition_graph(50, 10, 0.2, 0.05, seed=42)
 
-gene_adj = nx.adjacency_matrix(gene_net)
+# set data paths
+pp_f = "../pre/data/bio-decagon-ppi.csv"
+dd_f = "../pre/data/bio-decagon-combo.csv"
+dp_f = "../pre/data/bio-decagon-targets.csv"
+ds_f = "../pre/data/bio-decagon-mono.csv"
+
+# original data collection holder
+p_set = set()                 # protein nodes
+d_set = set()                 # drug nodes
+combo_set = set()             # edge types
+mono_set = set()              # additional drug features
+
+# read file to list
+pp_list, ddt_list, dp_list, ds_list = [], [], [], []   # lists of tuples
+
+# set temporary variable
+a, b, c = 0, 0, 0
+
+with open(pp_f, 'r') as f:
+    ppi = csv.reader(f)
+    next(ppi)
+    for [g1, g2] in ppi:
+        a, b = int(g1), int(g2)
+        p_set.add(a)
+        p_set.add(b)
+        pp_list.append((a, b))
+print("{:d} protein nodes indexed from {:d} to {:d}.".format(len(p_set), min(p_set), max(p_set)))
+print("{:d} protein-protein edges".format(len(pp_list)))
+
+with open(dd_f, "r") as f:
+    ppi = csv.reader(f)
+    next(ppi)
+    for [d1, d2, t, n] in ppi:
+        a, b, c = int(t.split('C')[-1]), int(d1.split('D')[-1]), int(d2.split('D')[-1])
+        combo_set.add(a)
+        d_set.add(b)
+        d_set.add(c)
+        ddt_list.append((b, c, a))
+print("{:d} drug nodes indexed from {:d} to {:d}".format(len(d_set), min(d_set), max(d_set)))
+print("{:d} drug-drug edges with {:d} edge types indexed from {:d} to {:d}"
+      .format(len(ddt_list), len(combo_set), min(combo_set), max(combo_set)))
+
+p_temp_set = set()
+d_temp_set = set()
+
+with open(dp_f, "r") as f:
+    ppi = csv.reader(f)
+    next(ppi)
+    for [d, p] in ppi:
+        a, b = int(d.split('D')[-1]), int(p)
+        d_set.add(a)
+        d_temp_set.add(a)
+        p_set.add(b)
+        p_temp_set.add(b)
+        dp_list.append((a, b))
+print("{:d} drug-protein edges".format(len(dp_list)))
+print("{:d} proteins not in the ppi network".format(p_set.__len__() - 19081))
+print("Protein numbers of {:d} indexed from {:d} to {:d}".format(len(p_temp_set), min(p_temp_set), max(p_temp_set)))
+print("Drug numbers of {:d} indexed from {:d} to {:d}".format(len(d_temp_set), min(d_temp_set), max(d_temp_set)))
+
+print("{:d} drugs not in the drug-drug network".format(d_set.__len__() - 645))
+
+
+temp = d_set.__len__()
+s_temp_set = set()
+d_temp_set = set()
+ds_list = []
+
+with open(ds_f, "r") as f:
+    ppi = csv.reader(f)
+    next(ppi)
+    for [d, e, n] in ppi:
+        a, b = int(e.split('C')[-1]), int(d.split('D')[-1])
+        mono_set.add(a)
+        d_set.add(b)
+        s_temp_set.add(a)
+        d_temp_set.add(b)
+        ds_list.append((b, a))
+print("{:d} drugs not in the drug-drug network."
+      .format(d_set.__len__() - temp))
+print("{:d} single drug side effects in drug pair side effects set."
+      .format((combo_set & mono_set).__len__()))
+
+print("Drug: {:d} indexed from {:d} to {:d}"
+      .format(len(d_temp_set), min(d_temp_set), max(d_temp_set)))
+print("Side effect: {:d} indexed from {:d} to {:d}"
+      .format(len(s_temp_set), min(s_temp_set), max(s_temp_set)))
+print("Edge: {:d}".format(len(ds_list)))
+
+
+# the numbers
+num_gene = p_set.__len__()
+num_drug = d_set.__len__()
+num_edge_type = combo_set.__len__()
+num_drug_additional_feature = mono_set.__len__()
+
+print("Summary: ")
+print(" -> Protein Node              : {:d}".format(num_gene))
+print(" -> Drug    Node              : {:d}".format(num_drug))
+print(" -> Drug    Pair  Side  Effect: {:d}".format(num_edge_type))
+print(" -> Single  Drug  Side  Effect: {:d}".format(num_drug_additional_feature))
+
+
+# protein
+gene_to_old = list(p_set)
+gene_to_new = sp.csr_matrix((range(num_gene), ([0] * num_gene, gene_to_old)))
+
+# drug
+drug_to_old = list(d_set)
+drug_to_new = sp.csr_matrix((range(num_drug), ([0] * num_drug, drug_to_old)))
+
+# drug pair side effect
+edge_type_to_old = list(combo_set)
+edge_type_to_new = sp.csr_matrix((range(num_edge_type), ([0] * num_edge_type, edge_type_to_old)))
+
+# single drug side effect
+side_effect_to_old = list(mono_set)
+side_effect_to_new = sp.csr_matrix((range(num_drug_additional_feature), ([0] * num_drug_additional_feature, side_effect_to_old)))
+
+# set temporary variable
+r, c, array_length = [], [], len(pp_list)
+
+for i in range(array_length):
+    r.append(gene_to_new[0, pp_list[i][0]])
+    c.append(gene_to_new[0, pp_list[i][1]])
+    
+gene_adj = sp.csr_matrix(([1] * array_length, (r, c)), shape=(num_gene, num_gene))
+
 gene_degrees = np.array(gene_adj.sum(axis=0)).squeeze()
 
-gene_drug_adj = sp.csr_matrix((10 * np.random.randn(n_genes, n_drugs) > 15).astype(int))
-drug_gene_adj = gene_drug_adj.transpose(copy=True)
+# set temporary variable
+r, c, array_length = [], [], len(dp_list)
 
+for i in range(array_length):
+    r.append(drug_to_new[0, dp_list[i][0]])
+    c.append(gene_to_new[0, dp_list[i][1]])
+
+drug_gene_adj = sp.csr_matrix(([1] * array_length, (r, c)), shape=(num_drug, num_gene))
+gene_drug_adj = drug_gene_adj.transpose(copy=True)
+
+# set temporary variable
+# r: key is the index of edge type, value is a list of two lists [drug list, drug list]
+r = {}        
+array_length = len(ddt_list)
+
+# build drug-drug network by edge types
+for i in range(array_length):
+    c = edge_type_to_new[0, ddt_list[i][2]]
+    if c not in r:
+        r[c] = [drug_to_new[0, ddt_list[i][0]]], [drug_to_new[0, ddt_list[i][1]]]
+    else:
+        r[c][0].append(drug_to_new[0, ddt_list[i][0]])
+        r[c][1].append(drug_to_new[0, ddt_list[i][1]])
+
+# build adjacency matrix
 drug_drug_adj_list = []
-tmp = np.dot(drug_gene_adj, gene_drug_adj)
-for i in range(n_drugdrug_rel_types):
-    mat = np.zeros((n_drugs, n_drugs))
-    for d1, d2 in combinations(list(range(n_drugs)), 2):
-        if tmp[d1, d2] == i + 4:
-            mat[d1, d2] = mat[d2, d1] = 1.
-    drug_drug_adj_list.append(sp.csr_matrix(mat))
+for i in range(num_edge_type):
+    drug_drug_adj_list.append(sp.csr_matrix(([1] * len(r[i][0]), (r[i][0], r[i][1])), shape=(num_drug, num_drug)))
+
 drug_degrees_list = [np.array(drug_adj.sum(axis=0)).squeeze() for drug_adj in drug_drug_adj_list]
 
+gene_feat = sp.identity(num_gene)
 
-# data representation
-adj_mats_orig = {
-    (0, 0): [gene_adj, gene_adj.transpose(copy=True)],
-    (0, 1): [gene_drug_adj],
-    (1, 0): [drug_gene_adj],
-    (1, 1): drug_drug_adj_list + [x.transpose(copy=True) for x in drug_drug_adj_list],
-}
-degrees = {
-    0: [gene_degrees, gene_degrees],
-    1: drug_degrees_list + drug_degrees_list,
-}
-
-# featureless (genes)
-gene_feat = sp.identity(n_genes)
 gene_nonzero_feat, gene_num_feat = gene_feat.shape
 gene_feat = preprocessing.sparse_to_tuple(gene_feat.tocoo())
 
-# features (drugs)
-drug_feat = sp.identity(n_drugs)
-drug_nonzero_feat, drug_num_feat = drug_feat.shape
-drug_feat = preprocessing.sparse_to_tuple(drug_feat.tocoo())
+# index feature
+r, c = list(range(num_drug)), list(range(num_drug))
+
+# additional feature append to index feature
+for (a, b) in ds_list:
+    r.append(drug_to_new[0, a])
+    c.append(side_effect_to_new[0, b] + num_drug)
+
+array_length = num_drug + len(ds_list)
+drug_feat_sparse = sp.csr_matrix(([1] * array_length, (r, c)), shape=(num_drug, num_drug + num_drug_additional_feature))
+
+drug_nonzero_feat, drug_num_feat = drug_feat_sparse.shape[1], np.count_nonzero(drug_feat_sparse.sum(axis=0))
+drug_feat = preprocessing.sparse_to_tuple(drug_feat_sparse.tocoo())
+
+val_test_size = 0.05
+# n_genes = 500
+# n_drugs = 400
+# n_drugdrug_rel_types = 3
+# gene_net = nx.planted_partition_graph(50, 10, 0.2, 0.05, seed=42)
+
+# gene_adj = nx.adjacency_matrix(gene_net)
+# gene_degrees = np.array(gene_adj.sum(axis=0)).squeeze()
+
+# gene_drug_adj = sp.csr_matrix((10 * np.random.randn(n_genes, n_drugs) > 15).astype(int))
+# drug_gene_adj = gene_drug_adj.transpose(copy=True)
+
+# drug_drug_adj_list = []
+# tmp = np.dot(drug_gene_adj, gene_drug_adj)
+# for i in range(n_drugdrug_rel_types):
+#     mat = np.zeros((n_drugs, n_drugs))
+#     for d1, d2 in combinations(list(range(n_drugs)), 2):
+#         if tmp[d1, d2] == i + 4:
+#             mat[d1, d2] = mat[d2, d1] = 1.
+#     drug_drug_adj_list.append(sp.csr_matrix(mat))
+# drug_degrees_list = [np.array(drug_adj.sum(axis=0)).squeeze() for drug_adj in drug_drug_adj_list]
+
+
+# # data representation
+# adj_mats_orig = {
+#     (0, 0): [gene_adj, gene_adj.transpose(copy=True)],
+#     (0, 1): [gene_drug_adj],
+#     (1, 0): [drug_gene_adj],
+#     (1, 1): drug_drug_adj_list + [x.transpose(copy=True) for x in drug_drug_adj_list],
+# }
+# degrees = {
+#     0: [gene_degrees, gene_degrees],
+#     1: drug_degrees_list + drug_degrees_list,
+# }
+
+# # featureless (genes)
+# gene_feat = sp.identity(n_genes)
+# gene_nonzero_feat, gene_num_feat = gene_feat.shape
+# gene_feat = preprocessing.sparse_to_tuple(gene_feat.tocoo())
+
+# # features (drugs)
+# drug_feat = sp.identity(n_drugs)
+# drug_nonzero_feat, drug_num_feat = drug_feat.shape
+# drug_feat = preprocessing.sparse_to_tuple(drug_feat.tocoo())
 
 # data representation
 num_feat = {
@@ -184,6 +366,7 @@ edge_types = {k: len(v) for k, v in adj_mats_orig.items()}
 num_edge_types = sum(edge_types.values())
 print("Edge types:", "%d" % num_edge_types)
 
+start = time.time()
 ###########################################################
 #
 # Settings and placeholders
@@ -300,3 +483,7 @@ for et in range(num_edge_types):
     print("Edge type:", "%04d" % et, "Test AUPRC score", "{:.5f}".format(auprc_score))
     print("Edge type:", "%04d" % et, "Test AP@k score", "{:.5f}".format(apk_score))
     print()
+
+end = time.time()
+time_taken = end - start
+print('This run has taken {} seconds to execute.'.format(time_taken))
